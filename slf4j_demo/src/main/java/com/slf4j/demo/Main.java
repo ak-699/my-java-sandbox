bb145b129828c7ad0e390b1227a153e2331096bc
ak-699 <kumar699.abhishek@gmail.com>
2025-09-09 20:41:54 +0530

    feat: sample logging code

--- slf4j_demo/src/main/java/com/slf4j/demo/Main.java
+++ slf4j_demo/src/main/java/com/slf4j/demo/Main.java
@@ -1,7 +1,12 @@
 package com.slf4j.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        logger.info("info level log");
     }
 }
