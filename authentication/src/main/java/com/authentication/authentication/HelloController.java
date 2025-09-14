f716867d7f27606044da2b448743caab6579517a
ak-699 <kumar699.abhishek@gmail.com>
2025-09-15 04:10:22 +0530

    feat: rbac demo

--- authentication/src/main/java/com/authentication/authentication/HelloController.java
+++ authentication/src/main/java/com/authentication/authentication/HelloController.java
@@ -1,5 +1,6 @@
 package com.authentication.authentication;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,18 @@ public class HelloController {
     @GetMapping("/")
     public String greeting() {
         return new String("hello");
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndpoint() {
+        return new String("hello user");
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndpoint() {
+        return new String("hello admin");
     }
 
 }
