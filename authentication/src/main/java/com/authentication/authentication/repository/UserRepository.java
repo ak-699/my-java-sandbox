1ecf4f09737b779130d36038da71e114562c23bf
ak-699 <kumar699.abhishek@gmail.com>
2025-09-15 06:49:04 +0530

    fix: rename user repo

--- authentication/src/main/java/com/authentication/authentication/repository/UserRepository.java
+++ authentication/src/main/java/com/authentication/authentication/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.authentication.authentication.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.authentication.authentication.entity.User;
 
-public interface UsersRepository extends JpaRepository<User, Long> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
